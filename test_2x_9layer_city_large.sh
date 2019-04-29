43cd1a6761596959a511bb500631840e1a3e45a7
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-04-29 19:42:44 +0000

    large shell scripts update

---
+++ test_2x_9layer_city_large.sh
@@ -0,0 +1,1 @@
+python test.py --dataroot ./datasets/cityscapes --name city_2x_9layer --model cycle_gan --netG resnet_9blocks --down_sample_nl 1 --load_size 144 --crop_size 128 --ngf 128 --ndf 128 --num_test 500 --eval
